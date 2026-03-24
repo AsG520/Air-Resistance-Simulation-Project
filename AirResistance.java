75077773b87ca65ea1311eb47944f870621ca7cd
AsG520 <asgirn520@gmail.com>
2026-03-24 18:16:44 -0400

    Setup of JFrame and Ball in following window.

--- AirResistance.java
+++ AirResistance.java
@@ -1,6 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.JLabel;
+
 public class AirResistance {
     public static void main(String[] args) {
-        double gravity = 9.8;
-        double mass = 5.0;
+        ImageIcon ballIcon = new ImageIcon("ball.png");
+        JLabel ball = new JLabel(ballIcon);
+        JFrame frame = new JFrame("Air Resistance Simulation");
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(ball);
     }
 }
